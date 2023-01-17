04a42c92b420175e43adb303101bf2e80c8fbe04
Dora Gores <d.lopesgores@gmail.com>
2023-01-17 12:23:35 +0000

    Finished 1.1.2: check_codeword

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -1,6 +1,19 @@
 require 'check_codeword'
+
 describe 'check_codeword' do 
   it 'returns "Correct! Come in." when given "horse"' do
     expect(check_codeword('horse')).to eq "Correct! Come in."
+  end
+end
+
+describe 'check_codeword' do 
+  it 'returns "Close, but nope." when given "hearse"' do
+    expect(check_codeword('hearse')).to eq "Close, but nope."
+  end
+end
+
+describe 'check_codeword' do 
+  it 'returns "WRONG!" when given "dog"' do
+    expect(check_codeword('dog')).to eq "WRONG!"
   end
 end
