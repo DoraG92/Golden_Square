1200d15f659d37e6490c3e22dfeaf1c8ad1ff013
Dora Gores <d.lopesgores@gmail.com>
2023-01-18 10:14:00 +0000

    Completed 1 test 2.1.1: Counter class

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -3,16 +3,14 @@ require 'counter'
 RSpec.describe Counter do 
   it 'adds two numbers to the count' do
     counter = Counter.new
-    counter.add(1)
-    report = counter.add(1)
-    expect(report).to eq 2 
+    counter.add(2)
+    expect(counter.instance_variable_get(:@count)).to eq 2
   end
 
   it 'gives a report of total counter so far' do
     counter = Counter.new
     result = counter.report
-  
-    expect(result).to eq "Counted to 2 so far."
+    expect(result).to eq "Counted to 0 so far."
   end
 end
 
