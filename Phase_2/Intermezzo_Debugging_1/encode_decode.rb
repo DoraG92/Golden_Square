df0a465d429f70fc1249b22a5502d1e203cf21f3
Dora Gores <d.lopesgores@gmail.com>
2023-01-19 16:54:44 +0000

    Debugging encode_decode

---
+++ Phase_2/Intermezzo_Debugging_1/encode_decode.rb
@@ -0,0 +1,29 @@
+# copied from another code, need to check the logic with coaches
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  ciphertext_chars = plaintext.chars.map do |char|
+    if cipher.find_index(char)
+      (65 + cipher.find_index(char)).chr
+    else
+      "Z"
+    end
+  end
+  return ciphertext_chars.join
+end
+# not touched
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+    cipher[65 - char.ord]
+  end
+  return plaintext_chars.join
+end
+# Intended output:
+#
+# > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+# => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+#
+# > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+# => "theswiftfoxjumpedoverthelazydog"
+
+puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
