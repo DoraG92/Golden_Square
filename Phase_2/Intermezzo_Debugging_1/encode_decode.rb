ab1acbe40be122c76d028bec687f62428d5152b4
Dora Gores <d.lopesgores@gmail.com>
2023-01-20 11:53:03 +0000

    Debugging exercise encode_decode

--- Phase_2/Intermezzo_Debugging_1/encode_decode.rb
+++ Phase_2/Intermezzo_Debugging_1/encode_decode.rb
@@ -1,23 +1,25 @@
 # copied from another code, need to check the logic with coaches
 def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
     if cipher.find_index(char)
       (65 + cipher.find_index(char)).chr
-    else
-      "Z"
     end
   end
   return ciphertext_chars.join
 end
 # not touched
 def decode(ciphertext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   plaintext_chars = ciphertext.chars.map do |char|
-    cipher[65 - char.ord]
+    if char.ord - 65 >= 25
+    else    
+        cipher[char.ord - 65]
+    end
   end
-  return plaintext_chars.join
+    return plaintext_chars.join
 end
+
 # Intended output:
 #
 # > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
@@ -27,3 +29,4 @@ end
 # => "theswiftfoxjumpedoverthelazydog"
 
 puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+puts decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
