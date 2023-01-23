177de7c221d8e268db723496db2b9419f10cd3f2
Dora Gores <d.lopesgores@gmail.com>
2023-01-23 10:26:05 +0000

    Refactored grammar_check

--- Phase_2/Test_Method/lib/grammar_check.rb
+++ Phase_2/Test_Method/lib/grammar_check.rb
@@ -1,9 +1,7 @@
 def grammar_check(text)
   if text[0] == text[0].upcase && text[-1] == "." || text[-1] == "?" || text[-1] == "!"
     return true
-  elsif text[0] == text[0].downcase && text[-1] == "."
-    return false
-  elsif text[0] == text[0].upcase && text[-1] != "." || "?" || "!"
+  else
     return false
   end
 end
